f02bfca9e16be7e0269482f33176ad805a52755e
NiratKashom <earthenly@gmail.com>
2021-07-24 11:33:18 +0700

    first commit

---
+++ space_travel/script.js
@@ -0,0 +1,13 @@
+// const allBtn = document.querySelectorAll('button');
+
+// console.log(allBtn);
+
+// allBtn.forEach(item => {
+//   item.addEventListener('click',event => {
+//       event.preventDefault();
+//      }   
+//   );
+// });
+
+// const listArrow = document.querySelectorAll('.schedule .schedule-table .row .arrow')
+// console.log(listArrow[1])
